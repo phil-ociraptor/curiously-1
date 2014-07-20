e1410e40b7f4b16f887ce19a56002d5af4810b7b
Jonathan Lau <jon@jon.sg>
2014-07-19 22:31:35 -0700

    Add validation

--- server/api/event/event.model.js
+++ server/api/event/event.model.js
@@ -1,12 +1,21 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    uniqueValidator = require('mongoose-unique-validator');
 
 var EventSchema = new Schema({
-  name: String,
-  code: String,
-  active: Boolean
+  name: { type: String, required: true, trim: true },
+  owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  code: { type: String, required: true, index: true, unique: true, validate: function(val) { return /^[a-z0-9\-]{4,15}$/g.test(val); } },
+  active: { type: Boolean, defaults: false }
 });
+
+EventSchema.plugin(uniqueValidator, { message: 'Error, expected {PATH} to be unique' });
+
+// EventSchema.schema.path('code').validate(function (value) {
+//   return /^[a-z0-9\-]{4,15}$/g.test(value);
+// }, 'Invalid code');
+
 
 module.exports = mongoose.model('Event', EventSchema);
--- server/api/event/index.js
+++ server/api/event/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./thing.controller');
+var controller = require('./event.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+//router.get('/', controller.index);
+//router.get('/:id', controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
+//router.put('/:id', controller.update);
+//router.patch('/:id', controller.update);
+//router.delete('/:id', controller.destroy);
 
 module.exports = router;
