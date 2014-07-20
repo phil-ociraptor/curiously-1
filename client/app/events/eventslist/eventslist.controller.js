89bb19f42a5232d3e7f8295b7d54dbad0087ac28
Jonathan Lau <jon@jon.sg>
2014-07-20 05:39:10 -0700

    Implement find event

--- client/app/events/eventslist/eventslist.controller.js
+++ client/app/events/eventslist/eventslist.controller.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('curiouslyApp')
-  .controller('EventsListCtrl', function ($scope, $http, Auth, User, Event) {
+  .controller('EventsListCtrl', function ($scope, $http, Auth, User, Event, $location) {
     $scope.isLoggedIn = Auth.isLoggedIn;
+    if(Auth.isLoggedIn()) {
+      Event.list(function(loadedEvents) {
+        $scope.events = loadedEvents
+      });
+    }
 
-    Event.list(function(loadedEvents) {
-      $scope.events = loadedEvents
-    });
+    $scope.go = function(form) {
+      $scope.noSuchEvent = false;
+      Event.get({code: $scope.eventCode.toLowerCase().replace(/ /g, '').replace(/[^a-z0-9\-]/g, '')}, function(loadedEvent) {
+        $location.path('/events/' + loadedEvent.code);
+      }, function(err) {
+        $scope.noSuchEvent = true;
+      });
+    }
 
   });
